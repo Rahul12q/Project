56aed12c1634035ad4c45fe6b862d30a5cfe2b5c
Rahul12q <rahul26shinde@gmail.com>
2021-12-12 19:42:25 +0530

    User Data Transfer Object file

---
+++ demo/src/main/java/com/example/demo/user/UserDTO.java
@@ -0,0 +1,43 @@
+package com.example.demo.user;
+
+public class UserDTO {
+    private String userName;
+
+    private String firstAndLastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstAndLastName() {
+        return firstAndLastName;
+    }
+
+    public void setFirstAndLastName(String firstAndLastName) {
+        this.firstAndLastName = firstAndLastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
